8bd6cfe86259fd7995e71df24d90e679c4978423
Scorpion <144194359+Scorpion7162@users.noreply.github.com>
2024-12-18 20:55:55 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Scorpion'
+description 'Script for LEO\'s to be able to grab the models of vehicles they are chasing'
+version '1.0.0'
+
+
+client_script 'modelgrabber.lua'
